31ffb4ad41bf4745ead2afeab930eaab13921a72
Ray Chen <jhihruei@users.noreply.github.com>
2024-08-24 10:27:47 +0800

    new: push to space by GitHub Actions

---
+++ .github/tools/deploy_to_space.py
@@ -0,0 +1,41 @@
+import argparse
+import subprocess
+import sys
+
+DEFAULT_USER = "jy-raychen"
+DEFAULT_SPACE_OWNER = "jy-raychen"
+DEFAULT_SPACE_NAME = "example"
+
+
+def force_push_to_remote(token, branch_name):
+    proc = subprocess.run(
+        [
+            "git",
+            "push",
+            "--force",
+            f"https://{DEFAULT_USER}:{token}@huggingface.co/spaces/{DEFAULT_SPACE_OWNER}/{DEFAULT_SPACE_NAME}",
+            f"{branch_name}:main",
+        ],
+        capture_output=True,
+    )
+
+    if proc.returncode == 0:
+        # NOTE: `git push` output always goes to stderr
+        results = proc.stderr.decode("utf-8")
+        return results
+
+    print(f"Failed to push remote: {proc.stderr.decode('utf-8')}")
+    return None
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Deploy to Hugging Face spaces.")
+    parser.add_argument("--branch_name", required=True)
+    parser.add_argument("--user_token", required=True)
+    args = parser.parse_args()
+
+    push_result = force_push_to_remote(args.user_token, args.branch_name)
+    if not push_result:
+        sys.exit(1)
+    print(push_result)
+    sys.exit(0)
